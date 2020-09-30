a0f56445174fce2bb1613862a3ad6a44abfadb69
Aletartaglia <alejandro.tartaglia92@gmail.com>
2020-09-30 09:49:25 -0300

    avanzamos un poco mas pero nos trabamos con el rendering y el save

--- routes/wiki.js
+++ routes/wiki.js
@@ -12,12 +12,33 @@ router.post('/', function (req, res, next) {
     title: req.body.title,
     content: req.body.content,
   })
-    .then(res.redirect("/"))
+    .then(res.json({
+      title: req.body.title,
+      content: req.body.content,
+    }))
+  page.save()
+    .then(savedPage => {
+      res.redirect(savedPage.route); // route virtual FTW
+    })
+    .catch(next);
+
 
 });
 router.get('/add', function (req, res, next) {
   res.render('addpage');
+
 });
 
+router.get('/:urlTitle', function (req, res, next) {
+  Page.findOne({
+    where: {
+      urlTitle: req.params.urlTitle
+    }
+  })
+    .then(function () {
+      res.render("wikipage.html", { page: req.params.urlTitle })
+    })
+    .catch(next);
+});
 
 module.exports = router;
